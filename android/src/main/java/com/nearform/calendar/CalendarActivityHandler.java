9aac8ec8547c46ab22a047c86d4941df97748786
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-02-27 12:02:27 +0100

    feat(android): check calendar

--- android/src/main/java/com/nearform/calendar/CalendarActivityHandler.java
+++ android/src/main/java/com/nearform/calendar/CalendarActivityHandler.java
@@ -2,24 +2,32 @@ package com.nearform.calendar;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CalendarContract;
 
 import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.Promise;
 
 import java.lang.RuntimeException;
 
+import com.nearform.calendar.CalendarEvent;
+
 public class CalendarActivityHandler extends BaseActivityEventListener {
     public static final int ADD_EVENT_REQUEST_CODE = 1;
     private Promise promise;
+    private CalendarEvent calendarEvent;
 
-    public void setPromise(Promise promise) {
+    public void init(final CalendarEvent calendarEvent, final Promise promise) {
+        this.calendarEvent = calendarEvent;
         this.promise = promise;
     }
 
     @Override
-    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(final Activity activity, final int requestCode, final int resultCode, final Intent data) {
         if (requestCode == ADD_EVENT_REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
+            if (resultCode == Activity.RESULT_OK || (resultCode == Activity.RESULT_CANCELED && this.isEventAdded(activity))) {
                 this.promise.resolve("EVENT_SAVED");
             } else if (resultCode == Activity.RESULT_CANCELED) {
                 this.promise.resolve("EVENT_CANCELLED");
@@ -27,5 +35,18 @@ public class CalendarActivityHandler extends BaseActivityEventListener {
                 this.promise.reject("ERR_UNKNOWN", new RuntimeException(Integer.toString(resultCode)));
             }
         }
+    }
+
+    private boolean isEventAdded(final Activity activity) {
+        final ContentResolver cr = activity.getContentResolver();
+        final Uri uri = CalendarContract.Events.CONTENT_URI;
+        final String selection = "((? = ?) AND (? = ?))";
+        final String[] selectionArgs = new String[]{CalendarContract.Events.TITLE, this.calendarEvent.name, CalendarContract.Events.DTSTART, String.valueOf(this.calendarEvent.startTime)};
+
+        final Cursor cur = cr.query(uri, null, selection, selectionArgs, null);
+        final int count = cur.getCount();
+        cur.close();
+
+        return count > 0;
     }
 }
--- android/src/main/java/com/nearform/calendar/CalendarManagerModule.java
+++ android/src/main/java/com/nearform/calendar/CalendarManagerModule.java
@@ -2,10 +2,8 @@ package com.nearform.calendar;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.icu.util.TimeZone;
 import android.provider.CalendarContract;
 
-import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -15,12 +13,13 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeMap;
 
 import com.nearform.calendar.CalendarActivityHandler;
+import com.nearform.calendar.CalendarEvent;
 
 public class CalendarManagerModule extends ReactContextBaseJavaModule {
     private static final int ADD_EVENT_REQUEST_CODE = 1;
     private final CalendarActivityHandler activityEventListener = new CalendarActivityHandler();
 
-    public CalendarManagerModule(ReactApplicationContext reactContext) {
+    public CalendarManagerModule(final ReactApplicationContext reactContext) {
         super(reactContext);
         reactContext.addActivityEventListener(this.activityEventListener);
     }
@@ -30,27 +29,21 @@ public class CalendarManagerModule extends ReactContextBaseJavaModule {
         return "CalendarManager";
     }
 
-    private TimeZone getTimeZone(ReadableMap eventDetails) {
-        return eventDetails.hasKey("timeZone") ? TimeZone.getTimeZone(eventDetails.getString("timeZone")) : TimeZone.getDefault();
-    }
-
-    private Intent createCalendarIntent(ReadableMap eventDetails) {
-        final Double startTime = eventDetails.getDouble("startTime");
-        final Double endTime = eventDetails.getDouble("endTime");
-
+    private Intent createCalendarIntent(final CalendarEvent calendarEvent) {
         return new Intent(Intent.ACTION_INSERT)
             .setData(CalendarContract.Events.CONTENT_URI)
-            .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTime.longValue())
-            .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.longValue())
-            .putExtra(CalendarContract.Events.TITLE, eventDetails.getString("name"))
-            .putExtra(CalendarContract.Events.EVENT_LOCATION, eventDetails.getString("location"))
-            .putExtra(CalendarContract.Events.EVENT_TIMEZONE, this.getTimeZone(eventDetails).getID());
+            .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, calendarEvent.startTime)
+            .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, calendarEvent.endTime)
+            .putExtra(CalendarContract.Events.TITLE, calendarEvent.name)
+            .putExtra(CalendarContract.Events.EVENT_LOCATION, calendarEvent.location)
+            .putExtra(CalendarContract.Events.EVENT_TIMEZONE, calendarEvent.timeZone.getID());
     }
 
     @ReactMethod
-    public void addEvent(ReadableMap eventDetails, Promise promise) {
-        final Intent calendarIntent = this.createCalendarIntent(eventDetails);
-        this.activityEventListener.setPromise(promise);
+    public void addEvent(final ReadableMap eventDetails, final Promise promise) {
+        final CalendarEvent calendarEvent = CalendarEvent.fromReadableMap(eventDetails);
+        final Intent calendarIntent = this.createCalendarIntent(calendarEvent);
+        this.activityEventListener.init(calendarEvent, promise);
 
         try {
             getCurrentActivity().startActivityForResult(calendarIntent, ADD_EVENT_REQUEST_CODE);
